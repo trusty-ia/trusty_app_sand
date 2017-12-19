94f68db22331422a445593ff805b72b624f8ba76
Zhang, Qi <qi1.zhang@intel.com>
2017-12-19 17:17:07 +0800

    [app][sand] LZMA test app
    Change-Id: Id9316af2ad1cd0234f137f85bf755f2a43d870a9
    Signed-off-by: Zhang, Qi <qi1.zhang@intel.com>

---
+++ lzma_test/manifest.c
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+#include <trusty_app_manifest.h>
+#include <stddef.h>
+#include <stdio.h>
+
+trusty_app_manifest_t TRUSTY_APP_MANIFEST_ATTRS trusty_app_manifest =
+{
+	.uuid = LZMA_TEST_APP_UUID,
+
+	.config_options =
+	/* optional configuration options here */
+	{
+		TRUSTY_APP_CONFIG_MIN_HEAP_SIZE(64*4096),
+
+		TRUSTY_APP_CONFIG_MIN_STACK_SIZE(2*4096),
+	},
+};
